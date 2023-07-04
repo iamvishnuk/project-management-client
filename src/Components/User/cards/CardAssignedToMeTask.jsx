a0de623d8ff1585c42e0a63526410a105b800b41
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2023-07-04 11:41:12 +0530

    delete some unwanted files and some design changes

--- src/Components/User/cards/CardAssignedToMeTask.jsx
+++ src/Components/User/cards/CardAssignedToMeTask.jsx
@@ -5,47 +5,48 @@ export const CardAssignedToMeTask = ({ assignedTask }) => {
     return (
         <>
             <div className="mt-5">
-                {assignedTask && // Check if assignedTask is defined
-                    assignedTask.map((value, index) => {
-                        return (
-                            <div key={index}>
-                                <h1 className="mb-2">
-                                    {value && value.boardName}{" "}
-                                </h1>
-                                <div className="grid gap-2">
-                                    {assignedTask &&
-                                        value.task.map((item, innerIndex) => {
-                                            // Renamed index to innerIndex
-                                            return (
-                                                <div
-                                                    className="h-14 rounded-md bg-gray-100 flex items-center px-2 hover:bg-gray-200"
-                                                    key={innerIndex}
-                                                >
-                                                    {item.taskType ===
+                {assignedTask &&
+                    assignedTask.map((boardGroup, index) => (
+                        <div key={index}>
+                            <h2 className="uppercase font-medium my-4">
+                                {boardGroup[0]?.projectId &&
+                                    boardGroup[0]?.projectId.projectName}
+                            </h2>
+                            {/* Render other project details */}
+                            {boardGroup.map((board) => (
+                                <div key={board._id} className="ml-5">
+                                    <h3 className="font-medium uppercase mb-2">
+                                        {board.boardName}
+                                    </h3>
+                                    {/* Render other board details */}
+                                    <ul className="ml-2 grid gap-2">
+                                        {board.task.map((task) => (
+                                            <li key={task._id} className="flex gap-2 bg-gray-100 p-2 hover:bg-gray-200">
+                                                    {task.taskType ===
                                                         "New task" && (
                                                         <RiTaskFill
                                                             color="blue"
                                                             size={25}
                                                         />
                                                     )}
-                                                    {item.taskType ===
+                                                    {task.taskType ===
                                                         "Bug" && (
                                                         <FaBug
                                                             color="red"
                                                             size={25}
                                                         />
                                                     )}
-                                                    <h1 className="ml-2">
-                                                        {item &&
-                                                            item.shortSummary}
-                                                    </h1>
-                                                </div>
-                                            );
-                                        })}
+                                                <p>
+                                                    {task.shortSummary}
+                                                </p>
+                                                {/* Render other task details */}
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            </div>
-                        );
-                    })}
+                            ))}
+                        </div>
+                    ))}
             </div>
         </>
     );
